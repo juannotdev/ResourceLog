77987989c1ceac7d5359764a86cd53beef32c581
juan <109624079+juannotdev@users.noreply.github.com>
2022-08-10 14:03:12 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Juan'
+description 'Logs whenever your resource starts, or stops'
+version '1.0'
+
+server_script 'server/server.lua'
+shared_script 'config.lua'
